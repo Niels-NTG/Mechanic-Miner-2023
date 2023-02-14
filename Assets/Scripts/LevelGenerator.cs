a614680368432ef1d99f6abdbb1955408c88f81e
Niels NTG <ntg999@gmail.com>
2023-02-14 20:59:36 +0100

    Implement basic level generator + player controller

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,41 +1,54 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using Random = UnityEngine.Random;
+using Random = System.Random;
 
 public class LevelGenerator : MonoBehaviour
 {
     private const int MIN_ELEMENT_COUNT = 2;
     private const int MAX_ELEMENT_COUNT = 20;
-
-    public Tile impassableTile;
-    public Tile borderTile;
-    public Tile spikeTile;
-    public Tile levelEntryTile;
-    public Tile levelExitTile;
-
-    public Tilemap tilemap;
-
+    
+    // Inner rect of the level measured in units of tiles.
     private RectInt levelSize = new RectInt(1, 1, 18, 13);
     
+    [SerializeField] private Tilemap impassableTilemap;
+    [SerializeField] private Tile impassableTile;
+    [SerializeField] private Tilemap spikesTilemap;
+    [SerializeField] private Tile spikeTile;
+    [SerializeField] private Tilemap entryTilemap;
+    [SerializeField] private Tile entryTile;
+    [SerializeField] private Tilemap exitTilemap;
+    [SerializeField] private Tile exitTile;
+
+    [SerializeField] private GameObject player;
     
+    [SerializeField] private int seed = 1289897231;
+    private Random rng;
 
     [ContextMenu("Generate")]
-    void Generate()
+    private void Generate()
     {
         Clear();
+
+        rng = new Random(seed);
         
-        for (int y = 0; y < levelSize.yMax + 1; y++)
-        {
-            tilemap.SetTile(new Vector3Int(0, y), borderTile);
-            tilemap.SetTile(new Vector3Int(levelSize.xMax, y), borderTile);
-        }
+        PlaceEntryAndExit();
 
-        for (int x = 0; x < levelSize.xMax + 1; x++)
+        int numberOfElements = RandomInt(MIN_ELEMENT_COUNT, MAX_ELEMENT_COUNT + 1);
+        for (int elementIndex = 0; elementIndex < numberOfElements; elementIndex++)
         {
-            tilemap.SetTile(new Vector3Int(x, 0), borderTile);
-            tilemap.SetTile(new Vector3Int(x, levelSize.yMax), borderTile);
+            if (RandomState())
+            {
+                PlaceBox();
+            }
+            else
+            {
+                PlaceLine();
+            }
         }
+    }
 
+    private void PlaceEntryAndExit()
+    {
         Vector3Int entryLocation;
         Vector3Int exitLocation;
         do
@@ -43,69 +56,92 @@ public class LevelGenerator : MonoBehaviour
             entryLocation = randomLocation();
             exitLocation = randomLocation();
         } while (entryLocation == exitLocation);
-        tilemap.SetTile(entryLocation, levelEntryTile);
-        tilemap.SetTile(exitLocation, levelExitTile);
+        entryTilemap.SetTile(entryLocation, entryTile);
+        exitTilemap.SetTile(exitLocation, exitTile);
 
-        int numberOfElements = Random.Range(MIN_ELEMENT_COUNT, MAX_ELEMENT_COUNT + 1);
-        Debug.Log(numberOfElements);
-        for (int elementIndex = 0; elementIndex < numberOfElements; elementIndex++)
+        player.transform.position = entryLocation + new Vector3(0.5f, 0.5f);
+    }
+
+    private void PlaceBox()
+    {
+        Vector3Int aa;
+        Vector3Int bb;
+        do
         {
-            Vector3Int aa;
-            Vector3Int bb;
-            Tile elementTileType;
-            
-            if (Random.value > 0.5f)
-            {
-                // Create box
-                aa = randomLocation();
-                bb = randomLocation();
-                elementTileType = impassableTile;
-            }
-            else
-            {
-                // Create line
-                aa = randomLocation();
-                if (Random.value > 0.5f)
-                {
-                    bb = new Vector3Int(aa.x, Random.Range(levelSize.yMin, levelSize.yMax));
-                }
-                else
-                {
-                    bb = new Vector3Int(Random.Range(levelSize.xMin, levelSize.xMax), aa.y);
-                }
-
-                elementTileType = spikeTile;
-            }
-            
+            aa = randomLocation();
+            bb = randomLocation();
             aa = Vector3Int.Min(aa, bb);
             bb = Vector3Int.Max(aa, bb);
-                
-            tilemap.BoxFill(aa, elementTileType, aa.x, aa.y, bb.x, bb.y);
+        } while (bb.x - aa.x < 3 || bb.y - aa.y < 3);
+
+        bool isOutline = RandomState();
+
+        RectInt box = new RectInt((Vector2Int)aa, (Vector2Int)(bb - aa));
+        foreach (Vector3Int point in box.allPositionsWithin)
+        {
+            if (isOutline)
+            {
+                if (point.x == box.xMin || point.y == box.yMin || point.x == box.xMax - 1 || point.y == box.yMax - 1)
+                {
+                    impassableTilemap.SetTile(point, impassableTile);        
+                }
+                continue;
+            }
+            impassableTilemap.SetTile(point, impassableTile);            
         }
     }
 
+    private void PlaceLine()
+    {
+        bool isVertical = RandomState();
+        Vector3Int aa = randomLocation();
+        int lineLength = isVertical
+            ? RandomInt(levelSize.yMin + 1, levelSize.yMax)
+            : RandomInt(levelSize.xMin + 1, levelSize.xMax);
+        bool isSpike = RandomState();
+
+        for (int i = isVertical ? aa.y : aa.x; i < lineLength; i++)
+        {
+            Vector3Int point = isVertical ? new Vector3Int(aa.x, i) : new Vector3Int(i, aa.y);
+            if (isSpike)
+            {
+                spikesTilemap.SetTile(point, spikeTile);
+            }
+            else
+            {
+                impassableTilemap.SetTile(point, impassableTile);
+            }
+        }
+    }
+
+    private bool RandomState()
+    {
+        return RandomInt(0, 2) == 1;
+    }
+    private int RandomInt(int min, int max)
+    {
+        return rng.Next(min, max);
+    }
+    
     private Vector3Int randomLocation()
     {
         return new Vector3Int(
-            Random.Range(levelSize.xMin, levelSize.xMax),
-            Random.Range(levelSize.yMin, levelSize.yMax)
+            RandomInt(levelSize.xMin, levelSize.xMax),
+            RandomInt(levelSize.yMin, levelSize.yMax)
         );
     }
 
     [ContextMenu("Clear")]
-    void Clear()
+    private void Clear()
     {
-        tilemap.ClearAllTiles();
+        impassableTilemap.ClearAllTiles();
+        spikesTilemap.ClearAllTiles();
+        entryTilemap.ClearAllTiles();
+        exitTilemap.ClearAllTiles();
     }
 
     private void Awake()
     {
-        tilemap.size = new Vector3Int(20, 15);
-    }
-
-    void Start()
-    {
         Generate();
     }
-
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,12 @@ public class PlayerController : MonoBehaviour
     // Part of this is based on https://gist.github.com/bendux/5fab0c176855d4e37bf6a38bb071b4a4
     
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpingPower = 8f;
 
     [SerializeField] private Rigidbody2D rigidBody;
-    [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-    
-    public LevelGenerator levelGenerator;
+    [SerializeField] private EdgeCollider2D groundCollider;
 
     // Update is called once per frame
     void Update()
@@ -23,11 +21,6 @@ public class PlayerController : MonoBehaviour
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpingPower);
         }
-
-        if (Input.GetButtonDown("Jump") && rigidBody.velocity.y > 0f)
-        {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y * 0.5f);
-        }
     }
 
     private void FixedUpdate()
@@ -37,6 +30,6 @@ public class PlayerController : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapBox(groundCheck.position, new Vector2(0.2f, 0.2f), groundLayer);
+        return Physics2D.IsTouchingLayers(groundCollider, groundLayer);
     }
 }
