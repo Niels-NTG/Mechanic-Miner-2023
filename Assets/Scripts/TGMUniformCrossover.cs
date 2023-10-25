754c61745c3c4dd03b09f7ea82ad1cb8214c2845
Niels NTG <ntg999@gmail.com>
2023-10-25 20:24:42 +0200

    Still replace gene if crossover doesn't detect same component field value type

--- Assets/Scripts/TGMUniformCrossover.cs
+++ Assets/Scripts/TGMUniformCrossover.cs
@@ -10,12 +10,20 @@ public sealed class TGMUniformCrossover : UniformCrossover
         IChromosome parent2 = parents[1];
         TGMChromosome chromosome1 = (TGMChromosome) parent1.CreateNew();
         TGMChromosome chromosome2 = (TGMChromosome) parent2.CreateNew();
-        if (chromosome1.isSameType(chromosome2))
+        bool isSameType = chromosome1.isSameType(chromosome2);
         {
             for (int index = 0; index < parent1.Length; index++)
             {
-                chromosome1.ReplaceGene(index, parent2.GetGene(index));
-                chromosome2.ReplaceGene(index, parent1.GetGene(index));
+                if (isSameType)
+                {
+                    chromosome1.ReplaceGene(index, parent2.GetGene(index));
+                    chromosome2.ReplaceGene(index, parent1.GetGene(index));
+                }
+                else
+                {
+                    chromosome1.ReplaceGene(index, parent1.GetGene(index));
+                    chromosome2.ReplaceGene(index, parent2.GetGene(index));
+                }
             }
         }
         return new List<IChromosome>
